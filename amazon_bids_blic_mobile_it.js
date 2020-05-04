1f1a68ffb60ad715859342b2abab0d5f3a7a272a
Joko Baucal <43583656+JokoBaucal@users.noreply.github.com>
2020-05-04 21:45:47 +0200

    Add files via upload

---
+++ amazon_bids_blic_mobile_it.js
@@ -0,0 +1,91 @@
+//load the apstag.js library
+! function (a9, a, p, s, t, A, g) {
+    if (a[a9]) return;
+
+    function q(c, r) {
+        a[a9]._Q.push([c, r])
+    }
+    a[a9] = {
+        init: function () {
+            q("i", arguments)
+        },
+        fetchBids: function () {
+            q("f", arguments)
+        },
+        setDisplayBids: function () {},
+        targetingKeys: function () {
+            return []
+        },
+        _Q: []
+    };
+    A = p.createElement(s);
+    A.async = !0;
+    A.src = t;
+    g = p.getElementsByTagName(s)[0];
+    g.parentNode.insertBefore(A, g)
+}("apstag", window, document, "script", "//c.amazon-adsystem.com/aax2/apstag.js");
+
+//initialize the apstag.js library on the page to allow bidding
+apstag.init({
+    pubID: '0d79d97f-1108-428c-8926-8b56782eacb6',
+    adServer: 'googletag'
+});
+
+apstag.fetchBids({
+    slots: [{
+            slotID: '320x50_P1',
+            slotName: '/161634325/Blic_Desktop/Blic_Mobile_IT/320x50_P1',
+            sizes: [
+                [320, 50], [320, 100], [300, 100],[300, 50]
+            ]
+        }, {
+            slotID: '320x50_P2',
+            slotName: '/161634325/Blic_Desktop/Blic_Mobile_IT/320x50_P2',
+            sizes: [
+                [300, 250], [336, 280], [320, 50], [320, 100], [300, 100],[300, 50]
+            ]
+        }, {
+            slotID: '320x50_P3',
+            slotName: '/161634325/Blic_Desktop/Blic_Mobile_IT/320x50_P3',
+            sizes: [
+                [300, 250], [336, 280], [320, 50], [320, 100], [300, 100],[300, 50]
+            ]
+        },
+        {
+            slotID: '320x50_P4',
+            slotName: '/161634325/Blic_Desktop/Blic_Mobile_IT/320x50_P4',
+            sizes: [
+                [300, 250], [336, 280], [320, 50], [320, 100], [300, 100],[300, 50]
+            ]
+        },
+        {
+            slotID: '320x50_P5',
+            slotName: '/161634325/Blic_Desktop/Blic_Mobile_IT/320x50_P5',
+            sizes: [
+                [300, 250], [336, 280], [320, 50], [320, 100], [300, 100],[300, 50]
+            ]
+        },
+        {
+            slotID: '320x50_P6',
+            slotName: '/161634325/Blic_Desktop/Blic_Mobile_IT/320x50_P6',
+            sizes: [
+                [300, 250], [336, 280], [320, 50], [320, 100], [300, 100],[300, 50]
+            ]
+        },
+        {
+            slotID: '320x50_P7',
+            slotName: '/161634325/Blic_Desktop/Blic_Mobile_IT/320x50_P7',
+            sizes: [
+                [300, 250], [336, 280], [320, 50], [320, 100], [300, 100],[300, 50]
+            ]
+        }
+
+    ],
+    timeout: 2e3
+}, function (bids) {
+    // set apstag targeting on googletag, then trigger the first DFP request in googletag's disableInitialLoad integration
+    googletag.cmd.push(function () {
+        apstag.setDisplayBids();
+        //googletag.pubads().refresh();
+    });
+});
---
+++ prebid_bids_blic_mobile_it.js
@@ -0,0 +1,595 @@
+//prebid bids
+
+var PREBID_TIMEOUT = 3000;
+var FAILSAFE_TIMEOUT = 3000;
+var adUnits = [{
+        code: '320x50_P1',
+        mediaTypes: {
+            banner: {
+                sizes: [
+                    [320, 100],
+                    [300, 50],
+                    [300, 100],
+                    [320, 50]
+                ]
+            }
+        },
+        bids: [{
+                bidder: 'ix',
+                params: {
+                    size: [320, 100],
+                    siteId: "336024"
+                }
+            }, {
+                bidder: 'ix',
+                params: {
+                    size: [320, 50],
+                    siteId: "336024"
+                }
+            }, {
+                bidder: 'ix',
+                params: {
+                    size: [300, 100],
+                    siteId: "336024"
+                }
+            }, {
+                bidder: 'ix',
+                params: {
+                    size: [300, 50],
+                    siteId: "336024"
+                }
+            }, {
+                bidder: "openx",
+                params: {
+                    unit: '540842239',
+                    delDomain: 'ringieraxelspr-d.openx.net'
+                }
+            },
+            {
+                bidder: 'rubicon',
+                params: {
+                    siteId: "235670",
+                    zoneId: "1162674",
+                    accountId: "19712"
+                }
+            }, {
+                bidder: 'criteo',
+                params: {
+                    networkId: '9517'
+                }
+            }, {
+                bidder: "adform",
+                params: {
+                    mid: '695226',
+                }
+            }, {
+                bidder: 'appnexus',
+                params: {
+                    placementId: "15028623"
+                }
+            }
+        ]
+    }, {
+        code: '320x50_P2',
+        mediaTypes: {
+            banner: {
+                sizes: [
+                    [300, 250],
+                    [320, 100],
+                    [300, 50],
+                    [300, 100],
+                    [320, 50]
+                ]
+            }
+        },
+        bids: [{
+            bidder: "openx",
+            params: {
+                unit: '540842243',
+                delDomain: 'ringieraxelspr-d.openx.net'
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [300, 250],
+                siteId: "336025"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [300, 50],
+                siteId: "336025"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [300, 100],
+                siteId: "336025"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [320, 50],
+                siteId: "336025"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [320, 100],
+                siteId: "336025"
+            }
+        }, {
+            bidder: 'criteo',
+            params: {
+                networkId: '9517'
+            }
+        }, {
+            bidder: 'connectad',
+            params: {
+                networkId: '10047',
+                siteId: '1033014'
+            }
+        }, {
+            bidder: 'rubicon',
+            params: {
+                siteId: "235670",
+                zoneId: "1162676",
+                accountId: "19712"
+            }
+        }, {
+            bidder: "adform",
+            params: {
+                mid: '695229',
+            }
+        }, {
+            bidder: 'appnexus',
+            params: {
+                placementId: "15028625"
+            }
+        }]
+    }, {
+        code: '320x50_P3',
+        mediaTypes: {
+            banner: {
+                sizes: [
+                    [300, 250],
+                    [320, 100],
+                    [300, 50],
+                    [300, 100],
+                    [320, 50]
+                ]
+            }
+        },
+        bids: [{
+            bidder: 'ix',
+            params: {
+                size: [300, 250],
+                siteId: "336026"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [300, 50],
+                siteId: "336026"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [300, 100],
+                siteId: "336026"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [320, 100],
+                siteId: "336026"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [320, 50],
+                siteId: "336026"
+            }
+        }, {
+            bidder: "openx",
+            params: {
+                mid: '540842248',
+                delDomain: 'ringieraxelspr-d.openx.net'
+            }
+        }, {
+            bidder: 'rubicon',
+            params: {
+                siteId: "235670",
+                zoneId: "1162678",
+                accountId: "19712"
+            }
+        }, {
+            bidder: 'criteo',
+            params: {
+                networkId: '9517'
+            }
+        }, {
+            bidder: 'connectad',
+            params: {
+                networkId: '10047',
+                siteId: '1033014'
+            }
+        }, {
+            bidder: "adform",
+            params: {
+                mid: '695230',
+            }
+        }, {
+            bidder: 'appnexus',
+            params: {
+                placementId: "15028627"
+            }
+        }]
+    },
+    {
+        code: '320x50_P4',
+        mediaTypes: {
+            banner: {
+                sizes: [
+                    [300, 250],
+                    [320, 100],
+                    [300, 50],
+                    [300, 100],
+                    [320, 50]
+                ]
+            }
+        },
+        bids: [{
+            bidder: 'ix',
+            params: {
+                size: [300, 250],
+                siteId: "336027"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [300, 50],
+                siteId: "336027"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [320, 50],
+                siteId: "336027"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [300, 100],
+                siteId: "336027"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [320, 100],
+                siteId: "336027"
+            }
+        }, {
+            bidder: "openx",
+            params: {
+                unit: '540842254',
+                delDomain: 'ringieraxelspr-d.openx.net'
+            }
+        }, {
+            bidder: 'rubicon',
+            params: {
+                siteId: "235670",
+                zoneId: "1162680",
+                accountId: "19712"
+            }
+        }, {
+            bidder: 'criteo',
+            params: {
+                networkId: '9517'
+            }
+        }, {
+            bidder: 'connectad',
+            params: {
+                networkId: '10047',
+                siteId: '1033014'
+            }
+        }, {
+            bidder: "adform",
+            params: {
+                mid: '695232',
+            }
+        }, {
+            bidder: 'appnexus',
+            params: {
+                placementId: "15028628"
+            }
+        }]
+    }, {
+        code: '320x50_P5',
+        mediaTypes: {
+            banner: {
+                sizes: [
+                    [300, 250],
+                    [320, 100],
+                    [300, 50],
+                    [300, 100],
+                    [320, 50]
+                ]
+            }
+        },
+        bids: [{
+            bidder: 'ix',
+            params: {
+                size: [300, 250],
+                siteId: "402068"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [300, 50],
+                siteId: "402068"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [300, 100],
+                siteId: "402068"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [320, 50],
+                siteId: "402068"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [320, 100],
+                siteId: "402068"
+            }
+        }, {
+            bidder: "openx",
+            params: {
+                unit: '540842226',
+                delDomain: 'ringieraxelspr-d.openx.net'
+            }
+        }, {
+            bidder: 'rubicon',
+            params: {
+                siteId: "235670",
+                zoneId: "1394332",
+                accountId: "19712"
+            }
+        }, {
+            bidder: 'criteo',
+            params: {
+                networkId: '9517'
+            }
+        }, {
+            bidder: 'connectad',
+            params: {
+                networkId: '10047',
+                siteId: '1033014'
+            }
+        }, {
+            bidder: "adform",
+            params: {
+                mid: '695233',
+            }
+        }, {
+            bidder: 'appnexus',
+            params: {
+                placementId: "15028623"
+            }
+        }]
+
+    }, {
+        code: '320x50_P6',
+        mediaTypes: {
+            banner: {
+                sizes: [
+                    [300, 250],
+                    [320, 100],
+                    [300, 50],
+                    [300, 100],
+                    [320, 50]
+                ]
+            }
+        },
+        bids: [{
+            bidder: 'ix',
+            params: {
+                size: [300, 250],
+                siteId: "430181"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [300, 50],
+                siteId: "430181"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [300, 100],
+                siteId: "430181"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [320, 50],
+                siteId: "430181"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [320, 100],
+                siteId: "430181"
+            }
+        }, {
+            bidder: "openx",
+            params: {
+                unit: '540950263',
+                delDomain: 'ringieraxelspr-d.openx.net'
+            }
+        }, {
+            bidder: 'rubicon',
+            params: {
+                siteId: "235670",
+                zoneId: "1493186",
+                accountId: "19712"
+            }
+        }, {
+            bidder: 'criteo',
+            params: {
+                networkId: '9517'
+            }
+        }, {
+            bidder: 'connectad',
+            params: {
+                networkId: '10047',
+                siteId: '1033014'
+            }
+        }, {
+            bidder: "adform",
+            params: {
+                mid: '802268',
+            }
+        }, {
+            bidder: 'appnexus',
+            params: {
+                placementId: "17835211"
+            }
+        }]
+
+    }, // odavde su nove pozicije
+    {
+        code: '320x50_P7',
+        mediaTypes: {
+            banner: {
+                sizes: [
+                    [300, 250],
+                    [320, 100],
+                    [300, 50],
+                    [300, 100],
+                    [320, 50]
+                ]
+            }
+        },
+        bids: [{
+            bidder: 'ix',
+            params: {
+                size: [300, 250],
+                siteId: "502323"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [300, 50],
+                siteId: "502323"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [300, 100],
+                siteId: "502323"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [320, 50],
+                siteId: "502323"
+            }
+        }, {
+            bidder: 'ix',
+            params: {
+                size: [320, 100],
+                siteId: "502323"
+            }
+        }, {
+            bidder: "openx",
+            params: {
+                unit: '541060467',
+                delDomain: 'ringieraxelspr-d.openx.net'
+            }
+        }, {
+            bidder: 'rubicon',
+            params: {
+                siteId: "235670",
+                zoneId: "1691574",
+                accountId: "19712"
+            }
+        }, {
+            bidder: 'criteo',
+            params: {
+                networkId: '9517'
+            }
+        }, {
+            bidder: 'connectad',
+            params: {
+                networkId: '10047',
+                siteId: '1033014'
+            }
+        }, {
+            bidder: "adform",
+            params: {
+                mid: '817480',
+            }
+        }, {
+            bidder: 'appnexus',
+            params: {
+                placementId: "19156403"
+            }
+        }]
+}
+];
+const customConfigObject = {
+    "buckets": [{
+        "precision": 2,
+        "min": 0,
+        "max": 4,
+        "increment": 0.01
+    }, {
+        "precision": 2,
+        "min": 4,
+        "max": 8,
+        "increment": 0.05
+    }, {
+        "precision": 2,
+        "min": 8,
+        "max": 20,
+        "increment": 0.1
+    }, {
+        "precision": 2,
+        "min": 21,
+        "max": 99,
+        "increment": 1.00
+    }]
+};
+
+var pbjs = pbjs || {};
+pbjs.que = pbjs.que || [];
+
+pbjs.que.push(function () {
+    pbjs.addAdUnits(adUnits);
+    pbjs.setConfig({
+        priceGranularity: customConfigObject
+    });
+    pbjs.requestBids({
+        bidsBackHandler: initAdserver,
+    });
+});
+
+function sendAdserverRequest() {
+    if (pbjs.adServerRequestSent) return;
+    pbjs.adServerRequestSent = true;
+    googletag.cmd.push(function () {
+        pbjs.que.push(function () {
+            pbjs.setTargetingForGPTAsync();
+            googletag.pubads().refresh();
+        });
+    });
+}
+setTimeout(function () {
+    sendAdServerRequest();
+}, PREBID_TIMEOUT);
